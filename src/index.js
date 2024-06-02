3bcda2b052933c1a6e4b2b271ff38ec4f6723007
Rajendra-2001 <rajendrakishore84@gmail.com>
2024-06-02 17:18:33 +0530

    Day 8

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@
 
 import connectDB from "./db/database.js";
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    console.log("MongoDB connected successfully!");
 
+    // Start the server after successful connection
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!!!", err);
+  });
 /*
 (async () => {
   try {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,14 @@
+const asyncHandler = (fn) => {
+  async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      resizeBy.status(err.code || 500).json({
+        sucess: false,
+        message: err.message,
+      });
+    }
+  };
+};
+
+export { asyncHandler };
