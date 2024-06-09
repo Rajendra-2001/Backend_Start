5466918f44a8be98d409c1454021a8f89c2a2628
Rajendra-2001 <rajendrakishore84@gmail.com>
2024-06-10 00:57:17 +0530

    Creating user Registration logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,20 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  //get user detail from frontEnd
+  //validation
+  //chech if user already exists
+  //check for image,check for avatar
+  // upload them to cloudinary
+  //create user object - create entry in DB
+  //remove password and refresh token field
+  //check for user creation
+  //return response
+
+  const { fullname, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  if (fullname === "") {
+  }
 });
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,14 +20,7 @@ const userSchema = new Schema(
       trim: true,
       // index: true,
     },
-    username: {
-      type: String,
-      required: true,
-      // unique: true,
-      // lowercase: true,
-      trim: true,
-      index: true,
-    },
+
     fullname: {
       type: String,
       required: true,
